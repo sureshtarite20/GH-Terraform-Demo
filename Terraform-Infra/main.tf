e1fd6c5c5e04546df9a0fee3dc82b9d9b0362943
sureshtarite <suresh.tarite@publicissapient.com>
2023-08-23 05:46:24 +0530

    infra changes01

--- Terraform-Infra/main.tf
+++ Terraform-Infra/main.tf
@@ -1,45 +1,64 @@
 terraform {
+  required_version = "~> 1.5.5"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.46.0"
+      version = " 3.69" # for production-grade
     }
   }
 }
-
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
-
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "TerraformGh-RG"
-    storage_account_name = "terraformghpoc"
-    container_name       = "tfstategh"
-    key                  = "dev.terraform.tfstate"
-  }
-}
-
 
 resource "azurerm_resource_group" "rg" {
   name     = var.rgname
   location = var.rglocation
 }
 
-resource "azurerm_virtual_network" "vnet1" {
-  name                = "${var.prefix}-10"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  location            = "${azurerm_resource_group.rg.location}"
-  address_space       = ["${var.vnet_cidr_prefix}"]
-}
 
-resource "azurerm_subnet" "subnet1" {
-  name                 = "subnet1"
-  virtual_network_name = "${azurerm_virtual_network.vnet1.name}"
-  resource_group_name  = "${azurerm_resource_group.rg.name}"
-  address_prefixes     = ["${var.subnet1_cidr_prefix}"]
-}
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "=2.46.0"
+#     }
+#   }
+# }
+
+# # Configure the Microsoft Azure Provider
+# provider "azurerm" {
+#   features {}
+# }
+
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "TerraformGh-RG"
+#     storage_account_name = "terraformghpoc"
+#     container_name       = "tfstategh"
+#     key                  = "dev.terraform.tfstate"
+#   }
+# }
+
+
+# resource "azurerm_resource_group" "rg" {
+#   name     = var.rgname
+#   location = var.rglocation
+# }
+
+# resource "azurerm_virtual_network" "vnet1" {
+#   name                = "${var.prefix}-10"
+#   resource_group_name = "${azurerm_resource_group.rg.name}"
+#   location            = "${azurerm_resource_group.rg.location}"
+#   address_space       = ["${var.vnet_cidr_prefix}"]
+# }
+
+# resource "azurerm_subnet" "subnet1" {
+#   name                 = "subnet1"
+#   virtual_network_name = "${azurerm_virtual_network.vnet1.name}"
+#   resource_group_name  = "${azurerm_resource_group.rg.name}"
+#   address_prefixes     = ["${var.subnet1_cidr_prefix}"]
+# }
 
 # resource "azurerm_network_security_group" "nsg1" {
 #   name                = "${var.prefix}-nsg1"
